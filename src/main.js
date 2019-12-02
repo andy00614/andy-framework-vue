36450ce66dfd1f7f4fbe7b9702d9f875c55564e3
andy00614 <33732843@qq.com>
2019-12-02 18:45:27 +0800

    name

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(VueRouter)
 Vue.use(ElementUI);
 Vue.config.productionTip = false
-// ddd
+// ddd name
 new Vue({
   render: h => h(App),
 }).$mount('#app')
